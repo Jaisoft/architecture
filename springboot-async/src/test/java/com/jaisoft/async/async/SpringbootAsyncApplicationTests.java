564026efb05cca3586edc4ff54d9f4c3c9bea9c1
Jaisoft <jaime.gomez.moraleda@gmail.com>
2023-02-16 10:16:37 +0100

    pocs added

---
+++ springboot-async/src/test/java/com/jaisoft/async/async/SpringbootAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaisoft.async.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
